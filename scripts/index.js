4a74161b3b8c3c295a3226f46ef7652c8c5bb100
EduSequelis1906 <edusequelis19@gmail.com>
2026-01-19 21:13:05 -0600

    funcion editar y guardar cambios en perfil implementados

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,62 @@ let initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
+const modalProfileEditBtn = document.querySelector(".profile__edit-button");
+const modalProfileForm = document.querySelector("#edit-popup");
+const modalClosePopUpBtn = modalProfileForm.querySelector(".popup__close");
+const modalTypeName = modalProfileForm.querySelector(".popup__input_type_name");
+const modalTypeDescription = modalProfileForm.querySelector(
+  ".popup__input_type_description"
+);
+
+const profileTitleText = document.querySelector(".profile__title");
+const profileDescriptionText = document.querySelector(".profile__description");
+
+function openModal(modalInput) {
+  modalInput.classList.add("popup_is-opened");
+}
+
+function closeModal(modalInput) {
+  modalInput.classList.remove("popup_is-opened");
+}
+
+modalClosePopUpBtn.addEventListener("click", () => {
+  closeModal(modalProfileForm);
+});
+modalProfileEditBtn.addEventListener("click", () => {
+  handleOpenEditModal(modalProfileForm);
+});
+
+function fillProfileForm() {
+  modalTypeName.value = profileTitleText.textContent;
+  modalTypeDescription.value = profileDescriptionText.textContent;
+}
+
+function handleOpenEditModal(modal) {
+  fillProfileForm();
+  openModal(modal);
+}
+
+const profileDisplay = document.querySelector(".profile__info");
+let formElement = document.querySelector("#edit-profile-form");
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  let nameInput = formElement.querySelector(".popup__input_type_name");
+  let jobInput = formElement.querySelector(".popup__input_type_description");
+
+  let name = nameInput.value;
+  let job = jobInput.value;
+
+  console.log(name);
+
+  let nameSet = profileDisplay.querySelector(".profile__title");
+  let jobSet = profileDisplay.querySelector(".profile__description");
+
+  nameSet.textContent = name;
+  jobSet.textContent = job;
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
