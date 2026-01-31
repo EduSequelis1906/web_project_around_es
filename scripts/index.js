7beef9291fab3935e8f25a2ada9db11b8def259e
EduSequelis1906 <edusequelis19@gmail.com>
2026-01-30 20:22:33 -0600

    Todas las funcionalidades del front end en produccion.

--- scripts/index.js
+++ scripts/index.js
@@ -25,20 +25,48 @@ let initialCards = [
   },
 ];
 
-initialCards.forEach(function (card) {
-  console.log(card.name);
-});
-
 const modalProfileEditBtn = document.querySelector(".profile__edit-button");
 const modalProfileForm = document.querySelector("#edit-popup");
 const modalClosePopUpBtn = modalProfileForm.querySelector(".popup__close");
 const modalTypeName = modalProfileForm.querySelector(".popup__input_type_name");
 const modalTypeDescription = modalProfileForm.querySelector(
-  ".popup__input_type_description"
+  ".popup__input_type_description",
 );
-
+const modalTemplateCard = document
+  .querySelector("#card")
+  .content.querySelector(".card");
+const cardContainer = document.querySelector(".cards__list");
 const profileTitleText = document.querySelector(".profile__title");
 const profileDescriptionText = document.querySelector(".profile__description");
+
+const createNewCardBtn = document.querySelector(".profile__add-button");
+const newCardPopUp = document.querySelector("#new-card-popup");
+const closePopUpNewCard = newCardPopUp.querySelector(".popup__close");
+const submitPopUpNewCard = newCardPopUp.querySelector(".popup__button");
+const newCardTitle = document.querySelector(".popup__input_type_card-name");
+const newCardLink = document.querySelector(".popup__input_type_url");
+const imageZoom = document.querySelector("#image-popup");
+
+createNewCardBtn.addEventListener("click", () => {
+  openModal(newCardPopUp);
+});
+
+closePopUpNewCard.addEventListener("click", function () {
+  closeModal(newCardPopUp);
+});
+
+newCardPopUp.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  handleCardFormSubmit(newCardTitle.value, newCardLink.value);
+
+  newCardTitle.value = "";
+  newCardLink.value = "";
+});
+
+function handleCardFormSubmit(title, link) {
+  renderCard(title, link, cardContainer);
+}
 
 function openModal(modalInput) {
   modalInput.classList.add("popup_is-opened");
@@ -65,6 +93,54 @@ function handleOpenEditModal(modal) {
   openModal(modal);
 }
 
+function getCardElement(name, link) {
+  const cardElement = modalTemplateCard.cloneNode(true); // clona el elemento card
+  const cardTitle = cardElement.querySelector(".card__title"); // selecciona el título del elemento card
+  const cardImage = cardElement.querySelector(".card__image"); // selecciona la imagen del elemento card
+  const cardlikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDltBtn = cardElement.querySelector(".card__delete-button");
+  const cardCloseBtn = imageZoom.querySelector(".popup__close");
+  const imageSetUpZoom = imageZoom.querySelector(".popup__image");
+  const imageSetUpCapt = imageZoom.querySelector(".popup__caption");
+
+  cardlikeBtn.addEventListener("click", function (evt) {
+    evt.target.classList.toggle("card__like-button_is-active");
+  });
+
+  cardDltBtn.addEventListener("click", function () {
+    cardElement.remove();
+  });
+
+  if (name === "") {
+    cardTitle.textContent = "Sin título";
+  } else {
+    cardTitle.textContent = name; // asigna la propiedad name del parámetro data
+  }
+
+  if (link === "") {
+    cardImage.src = "./images/placeholder.jpg";
+    cardImage.alt = "place holder";
+  } else {
+    cardImage.src = link;
+    cardImage.alt = "park image";
+  }
+
+  cardImage.addEventListener("click", function () {
+    imageSetUpZoom.src = link;
+    imageSetUpCapt.textContent = name;
+    openModal(imageZoom);
+  });
+  cardCloseBtn.addEventListener("click", function () {
+    closeModal(imageZoom);
+  });
+  return cardElement;
+}
+
+function renderCard(name, link, div) {
+  const newCard = getCardElement(name, link);
+  div.prepend(newCard);
+}
+
 const profileDisplay = document.querySelector(".profile__info");
 let formElement = document.querySelector("#edit-profile-form");
 
@@ -87,3 +163,9 @@ function handleProfileFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+initialCards.forEach(function (card) {
+  const names = card.name;
+  const links = card.link;
+  renderCard(names, links, cardContainer);
+});
